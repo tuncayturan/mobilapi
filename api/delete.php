88625fa539929380146727c861dc7ad34b42d2b7
Tuncay Turan <71794052+tuncayturan@users.noreply.github.com>
2023-03-29 11:48:04 +0300

    Update delete.php

--- api/delete.php
+++ api/delete.php
@@ -3,7 +3,7 @@ include("config.php");
 
 $id = $_GET['id'];
 
-$result = mysqli_query($mysqli, "DELETE FROM ogrenciler WHERE id=$id");
+$result = mysqli_query($baglan, "DELETE FROM ogrenciler WHERE id=$id");
 
 header("Location:index.php");
 ?>
