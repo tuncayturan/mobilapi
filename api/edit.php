8061bd8cf0183a8b965ecb072c581f9019145a3f
Tuncay Turan <71794052+tuncayturan@users.noreply.github.com>
2023-03-29 12:05:52 +0300

    Update edit.php

--- api/edit.php
+++ api/edit.php
@@ -43,10 +43,22 @@ while($res = mysqli_fetch_array($result))
 	$numara = $res['numara'];
 }
 ?>
-<html>
-<head>	
-	<title>Veri düzenle</title>
-		<meta charset="utf-8"/>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD Php Mysql</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </head>
 
 <body>
@@ -56,16 +68,16 @@ while($res = mysqli_fetch_array($result))
 	<form name="form1" method="post" action="edit.php">
 		<table border="0">
 			<tr> 
-				<td>Ad Soyad</td>
-				<td><input type="text" name="name" value="<?php echo $name;?>"></td>
+				<td><label for="adsoyad" class="form-label">Ad Soyad</label></td>
+				<td><input type="text" name="name" class="form-control" placeholder="Adınız" value="<?php echo $name;?>"></td>
 			</tr>
 			<tr> 
 				<td>numara</td>
-				<td><input type="text" name="no" value="<?php echo $numara;?>"></td>
+				<td><input type="text" name="no" class="form-control" placeholder="Numara" value="<?php echo $numara;?>"></td>
 			</tr>
 			<tr>
-				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
-				<td><input type="submit" name="update" value="Update"></td>
+				<td><input type="hidden" class="form-control" name="id" value=<?php echo $_GET['id'];?>></td>
+				<td><input type="submit" class="btn btn-warning" name="update" value="Update"></td>
 			</tr>
 		</table>
 	</form>
