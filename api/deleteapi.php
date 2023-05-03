a8857094e47959fdf6084150bfd539d60a179969
Tuncay Turan <71794052+tuncayturan@users.noreply.github.com>
2023-05-03 09:57:13 +0300

    Update deleteapi.php

--- api/deleteapi.php
+++ api/deleteapi.php
@@ -1,5 +1,5 @@
 <?php
-$baglanti=mysqli_connect("localhost","u724691799_tuncayx","Tuncay23*","u724691799_dbname");
+$baglanti=mysqli_connect("sql448.main-hosting.eu","u724691799_tuncayx","Tuncay23*","u724691799_dbname");
 $id=$_POST["id"];
 $sorgu="delete FROM ogrenciler where id='{$id}'";
 $komut=mysqli_query($baglanti,$sorgu);
